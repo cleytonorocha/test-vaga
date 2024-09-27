d159be4a34d0e67b7daef1dfeaa9b3a1f436ad8a
cleytonorocha <cleytoodeoliveirarocha@gmail.com>
2024-09-27 19:09:59 -0300

    feat: terceira questao resolvida

--- src/main/java/entidade/FaturamentoDiario.java
+++ src/main/java/entidade/FaturamentoDiario.java
@@ -1,3 +1,28 @@
-package main.java.entidade;
+package entidade;
 
-public record FaturamentoDiario(String dia, Double valor) {}
+public class FaturamentoDiario {
+    public String dia;
+    public Double valor;
+
+    public FaturamentoDiario(String dia, Double valor) {
+        this.dia = dia;
+        this.valor = valor;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+}
